de1d88edf6f1aa3c2ebf319158e7a486867467f3
yolisobral <yolisobrallores@gmail.com>
2026-03-01 19:42:50 +0100

    feat(types): :sparkles: añadir todas las clases profundizando en la clase ollivanders

---
+++ src/types.py
@@ -0,0 +1,23 @@
+class OllivanderShop():
+
+    def __init__(self,items):
+        self.items = items
+
+    def updateQuality(self):
+        for item in self.items:
+            item.updateQuality()
+    
+class Interfaz():
+    pass
+class Item:
+    pass
+class NormalItem(Interfaz,Item):
+    pass
+class Sulfuras(NormalItem):
+    pass
+class Conjured(NormalItem):
+    pass
+class Backstage(NormalItem):
+    pass
+class AgedBrie(NormalItem):
+    pass
